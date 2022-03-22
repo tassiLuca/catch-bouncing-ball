b581e43a1973dce16a843ff33631482ce60b48df
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-03-22 17:24:24 +0100

    added setup.h

---
+++ src/sketch_mar22c/setup.h
@@ -0,0 +1,16 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+#define L1_PIN 0
+#define L2_PIN 1
+#define L3_PIN 2
+#define L4_PIN 3
+#define LS_PIN 4
+
+#define POT_PIN A0
+#define T1_PIN 8
+#define T2_PIN 9
+#define T3_PIN 10
+#define T4_PIN 11
+
+#endif
